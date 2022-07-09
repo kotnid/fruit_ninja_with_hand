23e64a38b8e8526daa2b6807c0edb6eec8052974
kotnid <tkt0506tkt@gmail.com>
2022-07-09 21:11:24 +0800

    fruit_ninja : Add start screen
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- fruit_ninja.py
+++ fruit_ninja.py
@@ -24,25 +24,42 @@ while True:
     ret , img = cap.read()
     hands , img = detector.findHands(img)
 
-    if len(hands) > 0:
-        lmList = hands[0]["lmList"]
+    if playing == 0 :
+        cv2.putText(img , "Play fruit ninja" ,  (600,300) , cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0) , 2 ,  cv2.LINE_AA)
+        cv2.rectangle(img , (400,400) , (700,550) ,(255,0,0) , 2)
+        
+        if len(hands) > 0:
+            lmList = hands[0]["lmList"]
 
-        if len(lmList) > 5:
-            x1 , y1 = lmList[8][0] ,lmList[8][1]
-            cv2.circle(img , (x1 , y1) , 30 , (255,0,255) , cv2.FILLED)
+            if len(lmList) > 5:
+                x1 , y1 = lmList[8][0] ,lmList[8][1]
+                cv2.circle(img , (x1 , y1) , 30 , (255,0,255) , cv2.FILLED)
+                if (400 <= x1 <= 700) and (400 <= y1 <= 550):
+                    playing = 1
+                    end_time = time()+30
 
-            if abs(x1-x_pos) < 20 and abs(y1-y_pos) < 20:
-                x_pos , y_pos = random.randint(400,800) , random.randint(100,500)
-                marks += 1 
+    elif playing == 1:
+        
+        if len(hands) > 0:
+            lmList = hands[0]["lmList"]
+
+            if len(lmList) > 5:
+                x1 , y1 = lmList[8][0] ,lmList[8][1]
+                cv2.circle(img , (x1 , y1) , 30 , (255,0,255) , cv2.FILLED)
+
+                if abs(x1-x_pos) < 20 and abs(y1-y_pos) < 20:
+                    x_pos , y_pos = random.randint(400,800) , random.randint(100,500)
+                    marks += 1 
                 
-                playsound.playsound('res/coin.mp3', True)
+                    playsound.playsound('res/coin.mp3', True)
             
             
-    cv2.circle(img , (x_pos,y_pos) , 40 , (0,255,34) , -1)
-    cv2.putText(img , str(marks) , (50,50) , cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0) , 2 ,  cv2.LINE_AA)
+            cv2.circle(img , (x_pos,y_pos) , 40 , (0,255,34) , -1)
+            cv2.putText(img , str(marks) , (50,50) , cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0) , 2 ,  cv2.LINE_AA)
 
-    remain_time = int(end_time - time())
-    cv2.putText(img , f"time remain : {remain_time}" , (1000,50) , cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0) , 2 ,  cv2.LINE_AA )
+            remain_time = int(end_time - time())
+            cv2.putText(img , f"time remain : {remain_time}" , (1000,50) , cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0) , 2 ,  cv2.LINE_AA)
+
     cv2.imshow("Img" , img)
     
     cv2.waitKey(1)

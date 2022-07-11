e1470254bf2552ec92b684f84f604c2c28b2f16e
kotnid <tkt0506tkt@gmail.com>
2022-07-11 14:45:09 +0800

    fruit_ninja : The fruit move projectile
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- fruit_ninja.py
+++ fruit_ninja.py
@@ -3,24 +3,31 @@ from cvzone.HandTrackingModule import HandDetector
 import random 
 import playsound
 from time import time 
+import threading
 
 print("=== starting the device pls wait ===")
 
 
 wCam , hCam = 1920 , 1080
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FPS, 30)
 cap.set(3 , wCam)
 cap.set(4 , hCam)
 
-detector = HandDetector( detectionCon=0.8 , minTrackCon=0.4,  maxHands=2)
+detector = HandDetector( detectionCon=0.8 , minTrackCon=0.4,  maxHands=1)
 
 marks = 0
 playing = 0
 
-x_pos , y_pos = random.randint(700,800) , random.randint(100,500)
+x_pos , y_pos = random.randint(300,500) , 1000
 end_time = time()+30
+
+y_v = -1000
+x_v = 100
+
+def play_music():
+    playsound.playsound('res/coin.mp3', True)
 
 while True:
     ret , img = cap.read()
@@ -57,6 +64,7 @@ while True:
                 cv2.circle(img , (x1 , y1) , 30 , (255,0,255) , cv2.FILLED)
                 if (400 <= x1 <= 700) and (400 <= y1 <= 550):
                     playing = 0
+                    marks = 0
                     
         else :
             if len(hands) > 0:
@@ -66,14 +74,26 @@ while True:
                     x1 , y1 = lmList[8][0] ,lmList[8][1]
                     cv2.circle(img , (x1 , y1) , 30 , (255,0,255) , cv2.FILLED)
 
-                    if abs(x1-x_pos) < 20 and abs(y1-y_pos) < 20:
-                        x_pos , y_pos = random.randint(400,800) , random.randint(100,500)
+                    if abs(x1-x_pos) < 50 and abs(y1-y_pos) < 50:
+                        y_v = -1000
+                        x_v = random.randint(-300,300)
+                        x_pos , y_pos = random.randint(300,500) , 1000
                         marks += 1 
-                
-                        playsound.playsound('res/coin.mp3', True)
+
+                        x = threading.Thread(target=play_music)
+                        x.start()
             
             
+            if x_pos > 2000 or  y_pos > 1100 or x_pos < 0 or y_pos < 0 :
+                y_v = -1000
+                x_v = random.randint(-300,300)
+                x_pos , y_pos = random.randint(300,500) , 1000
+
+    
+            y_pos += int(y_v *(1/30))
+            x_pos += int(x_v * (1/30))
             cv2.circle(img , (x_pos,y_pos) , 40 , (0,255,34) , -1)
+            y_v = y_v + 9.81 * 2
             cv2.putText(img , str(marks) , (50,50) , cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0) , 2 ,  cv2.LINE_AA)
 
             
